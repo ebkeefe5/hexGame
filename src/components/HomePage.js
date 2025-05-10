fdeccd9819f227c78eb06d2d9b4491e9fa6da524
unknown <ebkeefe5@gmail.com>
2025-05-10 17:57:50 -0600

    fix typo

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,7 +5,7 @@ export default function HomePage() {
     <div class ="instructions leftIndent">
       <h2>PLAY HEX FREE</h2>
       <p>This is an alternating turn game. Red has the first move, but may not move in the center on the first turn. Red wins by connecting the bottom and top of the board with an unbroken chain of red hexagons. Blue wins by connecting the left and right side of the board with an unbroken chain of blue hexagons.</p> 
-      <p>The pictured below shows examples of red and blue winning, respectively.</p>
+      <p>The pictures below shows examples of red and blue winning, respectively.</p>
       <img src = "images/winningRedBoard.PNG" alt="An example winning red board" class = "image"></img>
       <img src = "images/winningBlueBoard.PNG" alt="An example winning red board" class = "image"></img>
     </div>
