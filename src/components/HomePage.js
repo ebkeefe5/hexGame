e882309a787ec9b03e3982679d2ed01c801875d0
unknown <ebkeefe5@gmail.com>
2025-05-10 18:10:47 -0600

    play hex free sounds fine

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function HomePage() {
   return (
     <div class ="instructions leftIndent">
-      <h2>PLAY HEX FREE</h2>
-      <p>This is an alternating turn game. Red has the first move, but may not move in the center on the first turn. Red wins by connecting the bottom and top of the board with an unbroken chain of red hexagons. Blue wins by connecting the left and right side of the board with an unbroken chain of blue hexagons.</p> 
+      <h2>Play Hex Free</h2>
+      <p>Hex is an alternating turn game. Red has the first move, but may not move in the center on the first turn. Red wins by connecting the bottom and top of the board with an unbroken chain of red hexagons. Blue wins by connecting the left and right side of the board with an unbroken chain of blue hexagons.</p> 
       <p>The pictures below shows examples of red and blue winning, respectively.</p>
       <img src = "images/winningRedBoard.PNG" alt="An example winning red board" class = "image"></img>
       <img src = "images/winningBlueBoard.PNG" alt="An example winning red board" class = "image"></img>
