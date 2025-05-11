8b0120764c5137a7a51d633d247848c23d6b6686
unknown <ebkeefe5@gmail.com>
2025-05-10 18:40:47 -0600

    update the description one more time

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function HomePage() {
   return (
     <div class ="instructions leftIndent">
-      <h2>Play Hex Now</h2>
-      <p>Hex is an alternating turn game. Red has the first move, but may not move in the center on the first turn. Red wins by connecting the bottom and top of the board with an unbroken chain of red hexagons. Blue wins by connecting the left and right side of the board with an unbroken chain of blue hexagons.</p> 
-      <p>The pictures below shows examples of red and blue winning, respectively.</p>
+      <h2>Play Hex Free</h2>
+      <p>Hex is an alternating turn board game. Red has the first move, but may not move in the center on the first turn. Red wins by connecting the bottom and top of the board with an unbroken chain of red hexagons. Blue wins by connecting the left and right side of the board with an unbroken chain of blue hexagons.</p> 
+      <p>Play against a friend, computer, or try a puzzle. The pictures below shows examples of red and blue winning, respectively.</p>
       <img src = "images/winningRedBoard.PNG" alt="An example winning red board" class = "image"></img>
       <img src = "images/winningBlueBoard.PNG" alt="An example winning red board" class = "image"></img>
     </div>
