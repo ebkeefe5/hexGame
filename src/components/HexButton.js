94e28769e1102e8f56665671d55c5cc6a780f68e
unknown <ebkeefe5@gmail.com>
2025-05-13 16:59:58 -0600

    allow the user to click on hexagons

--- src/components/HexButton.js
+++ src/components/HexButton.js
@@ -4,7 +4,7 @@ import { HEXAGON_EDGE_LENGTH, TOP_LEFT_HEXAGON_CENTER_X, TOP_LEFT_HEXAGON_CENTER
 export default class HexButton extends React.Component
 {
     render() {
-        const { row, col, onClick} = this.props;
+        const { row, col, fill, onClick} = this.props;
 
         var cx = TOP_LEFT_HEXAGON_CENTER_X + col * Math.sqrt(3) * HEXAGON_EDGE_LENGTH + Math.sqrt(3)*HEXAGON_EDGE_LENGTH/2*row;
         var cy = TOP_LEFT_HEXAGON_CENTER_y + row * 3/2 * HEXAGON_EDGE_LENGTH;
@@ -32,7 +32,7 @@ export default class HexButton extends React.Component
         };
 
         const polygonStyle = {
-            fill: '#A9A9A9',
+            fill: fill,
             stroke: 'black',
             strokeWidth: 0.5,
         };
--- src/components/TwoPlayerPage.js
+++ src/components/TwoPlayerPage.js
@@ -1,22 +1,43 @@
 import React from 'react';
+import { useState } from 'react';
 import HexButton from './HexButton';
+import { BOARD_DIMENSION } from '../constants.js';
+import { COLORS } from '../constants.js';
 
-export default function TwoPlayerPage() { 
+export default function TwoPlayerPage() {
+    const [hexagons, setHexagons] = useState(Array(BOARD_DIMENSION*BOARD_DIMENSION).fill(0)); 
 
-    const handleHexagonClick = () => {
-        console.log("Hexagon clicked on the TwoPlayerPage!");
-        // Add your click handling logic here
+    const handleHexagonClick = (i, j) => {
+        console.log("Hexagon clicked on the TwoPlayerPage!"); 
+        const nextHexagons = hexagons.slice();
+        nextHexagons[i * BOARD_DIMENSION + j] = 1;
+        setHexagons(nextHexagons); 
     };
 
+    const renderHexagons = () => {
+        const hexagonComponents = [];
+        for (let i = 0; i < BOARD_DIMENSION; i++) {
+            for (let j = 0; j < BOARD_DIMENSION; j++) {
+                var fill = COLORS[0];
+                if (hexagons[i * BOARD_DIMENSION + j] == 1)
+                    fill = COLORS[1];
+                hexagonComponents.push(<HexButton 
+                    key={`hex-${i}-${j}`}
+                    row = {i}
+                    col = {j}
+                    fill = {fill}
+                    onClick={() => handleHexagonClick(i, j)}
+            />)
+            }
+        }
+        return hexagonComponents;
+    }
+
     return (
-    <div className = "leftIndent">
-        <svg viewBox="0 0 200 200">
-            <HexButton 
-                row = {0}
-                col = {0}
-                onClick={() => this.handleHexagonClick()}
-            />           
-        </svg>
-    </div>
+        <div className = "leftIndent">
+            <svg viewBox="0 0 200 200">
+                 {renderHexagons()}       
+            </svg>
+        </div>
     );
 }
