66af63d72373b840aaa6566275ab4a43ac6c7c69
unknown <ebkeefe5@gmail.com>
2025-05-13 22:35:36 -0600

    deal with center piece

--- src/components/TwoPlayerPage.js
+++ src/components/TwoPlayerPage.js
@@ -3,20 +3,30 @@ import { useState } from 'react';
 import HexButton from './HexButton';
 import { BOARD_DIMENSION } from '../constants.js';
 import { COLORS } from '../constants.js';
+import { NOT_ALLOWED_COLOR } from '../constants.js';
 
 export default function TwoPlayerPage() {
-    const [hexagons, setHexagons] = useState(Array(BOARD_DIMENSION*BOARD_DIMENSION).fill(0)); 
+    const CENTER_INDEX = Math.floor((BOARD_DIMENSION * BOARD_DIMENSION)/2);
+    const [hexagons, setHexagons] = useState(() => {
+        const initialHexagons = Array(BOARD_DIMENSION * BOARD_DIMENSION).fill(0);
+        initialHexagons[CENTER_INDEX] = -1;
+        return initialHexagons;
+  });
     const [redIsNext, setRedIsNext] = useState(true);
 
     const handleHexagonClick = (i, j) => {
         if (hexagons[i * BOARD_DIMENSION + j] != 0)
-            return;
+            return
         
+
         const nextHexagons = hexagons.slice();
         if (redIsNext)
             nextHexagons[i * BOARD_DIMENSION + j] = 1;
         else 
             nextHexagons[i * BOARD_DIMENSION + j] = 2;
+        if (nextHexagons[CENTER_INDEX] == -1)
+            nextHexagons[CENTER_INDEX] = 0;
+        
         setRedIsNext(!redIsNext);
         setHexagons(nextHexagons); 
     };
@@ -30,6 +40,11 @@ export default function TwoPlayerPage() {
                     fill = COLORS[1];
                 else if (hexagons[i * BOARD_DIMENSION + j] == 2)
                     fill = COLORS[2];
+                else if (hexagons[i * BOARD_DIMENSION + j] == -1)
+                {
+                    fill = NOT_ALLOWED_COLOR;
+                }
+                    
                 hexagonComponents.push(<HexButton 
                     key={`hex-${i}-${j}`}
                     row = {i}
