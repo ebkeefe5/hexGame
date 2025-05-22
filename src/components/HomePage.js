fdf67bfade69016e66de49f0079babea577ee523
unknown <ebkeefe5@gmail.com>
2025-05-22 10:44:48 -0600

    try using an svg view box for images on homepage, to fix mobile bug

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,9 +7,29 @@ export default function HomePage() {
       <p style = {{marginLeft:'5%'}}>Hex is an alternating turn board game. Red has the first move, but may not move in the center on the first turn. Red wins by connecting the bottom and top of the board with an unbroken chain of red hexagons. Blue wins by connecting the left and right side of the board with an unbroken chain of blue hexagons.</p> 
       <p style = {{marginLeft:'5%'}}>Play against a friend, computer, or try a puzzle. The pictures below shows examples of red and blue winning, respectively.</p>
       <br></br>
-      <img src = "images/winningRedBoard.PNG" alt="An example winning red board" className = "image"></img>
-      <img src = "images/winningBlueBoard.PNG" alt="An example winning red board" className = "image"></img>
-      <svg viewBox='0 0 1000 800'>     
+      <svg viewBox='0 0 1000 800'>    
+        {/* Red Board */}
+        <image
+          href={`/images/winningRedBoard.PNG`} // Use href for SVG image
+          x="0"          // X-coordinate within the SVG viewBox
+          y="0"          // Y-coordinate within the SVG viewBox
+          width="500"    // Width of the image within the SVG viewBox
+          height="400"   // Height of the image within the SVG viewBox
+          alt="An example winning red board" // alt text is not directly supported by SVG image, consider ARIA or a wrapping title
+        />
+
+        {/* Blue Board */}
+        <image
+          href={`/images/winningBlueBoard.PNG`}
+          x="500"        // X-coordinate to place it next to the first image
+          y="0"
+          width="500"
+          height="400"
+          alt="An example winning blue board"
+        />
+      <title>Winning Boards Display</title>
+      <desc>Two example winning game boards: one red, one blue.</desc>
+       
       </svg>
     </div>
   );
