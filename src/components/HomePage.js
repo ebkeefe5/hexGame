7cb54800b684435b270401b91ee5937d0eccfa85
unknown <ebkeefe5@gmail.com>
2025-05-21 21:40:04 -0600

    add min width

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function HomePage() {
   return (
-    <div style={{ margin: 'auto', width:'30%' }}>
+    <div style={{ margin: 'auto', width:'30%', minWidth:'300px' }}>
       <h2 style = {{marginLeft:'5%'}}>Play Hex Free</h2>
       <p style = {{marginLeft:'5%'}}>Hex is an alternating turn board game. Red has the first move, but may not move in the center on the first turn. Red wins by connecting the bottom and top of the board with an unbroken chain of red hexagons. Blue wins by connecting the left and right side of the board with an unbroken chain of blue hexagons.</p> 
       <p style = {{marginLeft:'5%'}}>Play against a friend, computer, or try a puzzle. The pictures below shows examples of red and blue winning, respectively.</p>
